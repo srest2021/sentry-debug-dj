758d01e45b3865ff5dfa71e03e4bff6e1bcb2a83
srest2021 <sofia.rest@sentry.io>
2025-08-18 11:03:06 -0700

    music player prefs

---
+++ static/app/components/musicPlayer/musicPlayerPreferences.tsx
@@ -0,0 +1,62 @@
+import localStorage from 'sentry/utils/localStorage';
+
+const LOCAL_STORAGE_KEY = 'music-player-config';
+
+export type MusicPlayerPrefs = {
+  defaultPlaylistId: string;
+  isEnabled: boolean;
+  shuffle: boolean;
+  volume: number;
+};
+
+const DEFAULT_PREFS: MusicPlayerPrefs = {
+  isEnabled: true,
+  defaultPlaylistId: 'debugging-beats',
+  shuffle: false,
+  volume: 0.7,
+};
+
+const DISABLED_PREFS: MusicPlayerPrefs = {
+  isEnabled: false,
+  defaultPlaylistId: 'debugging-beats',
+  shuffle: false,
+  volume: 0.7,
+};
+
+export interface MusicPlayerPrefsStrategy {
+  _prefs: MusicPlayerPrefs;
+  get: () => MusicPlayerPrefs;
+  set: (prefs: MusicPlayerPrefs) => void;
+}
+
+export const StaticMusicPlayerPreferences: MusicPlayerPrefsStrategy = {
+  _prefs: {...DEFAULT_PREFS},
+  get() {
+    return this._prefs;
+  },
+  set(prefs) {
+    this._prefs = prefs;
+  },
+};
+
+export const StaticDisabledMusicPlayerPreferences: MusicPlayerPrefsStrategy = {
+  _prefs: {...DISABLED_PREFS},
+  get() {
+    return this._prefs;
+  },
+  set(prefs) {
+    this._prefs = prefs;
+  },
+};
+
+export const LocalStorageMusicPlayerPreferences: MusicPlayerPrefsStrategy = {
+  _prefs: {...DEFAULT_PREFS},
+  get() {
+    const parsed = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || '{}');
+    return {...DEFAULT_PREFS, ...parsed};
+  },
+  set(prefs) {
+    this._prefs = prefs;
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(prefs));
+  },
+};
